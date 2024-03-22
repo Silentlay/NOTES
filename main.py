abf66e6fad8431dc801ed90c8cb82c094748ae82
Silentlay <gerka185@gmail.com>
2024-03-22 05:51:23 +0300

    format main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ class Note:
         self.note_id = note_id
         self.title = title
         self.body = body
-        self.created_at = created_at if created_at else datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.updated_at = updated_at if updated_at else datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.created_at = created_at if created_at else datetime.now().strftime("%Y-%m-%d %H:%M:%S")("%d-%m-%Y %S:%M:%H")
+        self.updated_at = updated_at if updated_at else datetime.now().strftime("%Y-%m-%d %H:%M:%S")("%d-%m-%Y %S:%M:%H")
 
     def __repr__(self):
         return f"Номер заметки: {self.note_id}\nНазвание: {self.title}\nТекст: {self.body}\nСоздана в: {self.created_at}\nОбновлена в: {self.updated_at}"
@@ -23,17 +23,21 @@ class NoteManager:
     def load_notes(self):
         if not os.path.exists(self.file_path):
             return []
-        with open(self.file_path, "r") as file:
+        with open(self.file_path, "r", encoding="utf-8") as file:
             notes_data = json.load(file)
             return [Note(**note_data) for note_data in notes_data]
 
     def save_notes(self):
-        with open(self.file_path, "w") as file:
-            json.dump([note.__dict__ for note in self.notes], file)
+        with open(self.file_path, "w", encoding="utf-8") as file:
+            json.dump([note.__dict__ for note in self.notes], file, ensure_ascii=False)
 
     def list_notes(self):
         for note in self.notes:
-            print(note)
+            print("Номер заметки:", note.note_id)
+            print("Заголовок:", note.title)
+            print("Текст заметки:", note.body)
+            print("Дата создания:", note.created_at)
+            print("Дата последнего изменения:", note.updated_at)
             print()
 
     def add_note(self, title, body):
@@ -80,12 +84,12 @@ def main():
         if choice == "1":
             note_manager.list_notes()
         elif choice == "2":
-            title = input("Введите названние заметки: ")
+            title = input("Введите заголовок для заметки: ")
             body = input("Введите текст: ")
             note_manager.add_note(title, body)
         elif choice == "3":
             note_id = int(input("Введите номер заметки: "))
-            title = input("Введите новое название: ")
+            title = input("Введите новый заголовок: ")
             body = input("Введите новый текст: ")
             note_manager.edit_note(note_id, title, body)
         elif choice == "4":
